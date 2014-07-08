6cccb678ddc773106ffb23117e22bf838be5e7d6
Diego Sanchez <diego.sanches@gmail.com>
2014-07-08 07:26:57 -0300

    My first directive

---
+++ directive.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('superhero', []); 
+ 
+myApp.directive( 'superman', function () {
+  return {
+    restrict: 'E',
+    template: "<div> Here I am to save the day </div>"
+  }
+});
+

cbcc95c942318be3b955b2e50276e9af8e2366ac
diego <diego.sanches@gmail.com>
2014-07-14 15:11:43 -0300

    templateUrl fixed

--- tabbed_view/tab_set.js
+++ tabbed_view/tab_set.js
@@ -1,14 +1,15 @@
 var module = angular.module("tabs", []);
 
-module.directive( 'mierda', function() {
+module.directive( 'myTabSet', function() {
   return {
     restrict: 'E',
     replace: true,
     transclude: true,
-    templateURL: 'tab_set.html',
-    scope: { name: '=name' },
+    templateUrl: 'tab_set.html',
+    scope: { name: '@name' },
     link: function(scope, element, attrs) {
-      console.debug( scope, element, attrs);
+      console.debug( scope);
+      console.debug( scope.$parent );
     }
   };
 });

b08e55d675673e543c865115508eb330094f35ad
diego <diego.sanches@gmail.com>
2014-07-29 12:49:59 -0300

    Testing environment

---
+++ angular-gap/test/karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function(config){
+  config.set({
+    hostname: '127.0.0.1',
+
+    basePath : '../',
+
+    files : [
+      'js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome_small'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    },
+
+    customLaunchers: {
+      Chrome_small: {
+        base: 'Chrome',
+        flags: ['--window-size=400,400']
+      }
+    }
+
+  });
+};
---
+++ angular-gap/test/unit/controllersSpec.js
@@ -0,0 +1,9 @@
+describe('PhoneListCtrl', function(){
+
+  beforeEach(module('phonecatApp'));
+
+  it('should create "phones" model with 3 phones', function() {
+    expect(3).toBe(3);
+  });
+
+});

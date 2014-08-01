61bad577dfa45b1feabe3c8cae98431ca476c13a
diego <diego.sanches@gmail.com>
2014-08-01 11:45:18 -0300

    Select added

--- angular-gap/js/app.js
+++ angular-gap/js/app.js
@@ -3,10 +3,15 @@ var app = angular.module( 'phonecatApp', [] );
 app.controller( 'PhoneListCtrl', function($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
+     'age': 1,
      'snippet': 'Fast just got faster with Nexus S.'},
     {'name': 'Motorola XOOM™ with Wi-Fi',
+     'age': 2,
      'snippet': 'The Next, Next Generation tablet.'},
     {'name': 'MOTOROLA XOOM™',
+     'age': 3,
      'snippet': 'The Next, Next Generation tablet.'}
   ];
+  $scope.orderProp = 'name';
+
 });
--- angular-gap/test/karma.conf.js
+++ angular-gap/test/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config){
 
     autoWatch : true,
 
-    frameworks: ['jasmine'],
+    frameworks:[ 'jasmine'],
 
     browsers : ['Chrome_small'],
 

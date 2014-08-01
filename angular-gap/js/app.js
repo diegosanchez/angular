8a826b08cf6cc8fdcf6f0f50983c6fbaf810d7fd
diego <diego.sanches@gmail.com>
2014-08-01 16:11:08 -0300

    XRH request added

--- angular-gap/js/app.js
+++ angular-gap/js/app.js
@@ -1,9 +1,10 @@
 var app = angular.module( 'phonecatApp', [] );
 
-app.controller( 'PhoneListCtrl', function($scope) {
+app.controller( 'PhoneListCtrl', function($scope, $http) {
   var xhr = $http.get( 'data/phones.json').success( function(data) {
     $scope.phones = data;
-  };
+  });
+
   $scope.orderProp = 'name';
 
 });

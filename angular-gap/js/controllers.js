efbc871e89f2ca6d434d6fc6303d6df3fdf8c3cb
Diego Sanchez <diego.sanches@gmail.com>
2014-08-07 07:04:16 -0300

    Event listener added

--- angular-gap/js/controllers.js
+++ angular-gap/js/controllers.js
@@ -1,4 +1,4 @@
-var controllers = angular.module( 'phonecatControllers', [] );
+var controllers = angular.module( 'phonecatControllers', ['phonecatFilters'] );
 
 controllers.controller( 'PhoneListCtrl', function($scope, $http) {
   var xhr = $http.get( 'data/phones.json').success( function(data) {
@@ -10,10 +10,14 @@ controllers.controller( 'PhoneListCtrl', function($scope, $http) {
 
 });
 
-
 controllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $http.get('data/phones/' + $routeParams.phoneId + '.json').success(function(data) {
       $scope.phone = data;
+      $scope.mainImageUrl = data.images[0];
+
+      $scope.setMainImage = function( inputImage ) {
+        $scope.mainImageUrl = inputImage;
+      };
     });
   }]);
